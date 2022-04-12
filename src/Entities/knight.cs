ae78d4c3695694bfb6e3d85e663ddda9a8262e7d
Tiago Lazarini <tiago_lazarini@hotmail.com>
2022-04-12 13:03:32 -0300

    Update knight.cs

--- src/Entities/knight.cs
+++ src/Entities/knight.cs
@@ -2,9 +2,6 @@ public class knight : Hero
 {
   public knight(string Name, int Level, string heroType, int MP, int HP) : base(Name, Level, heroType, MP, HP)
   {
-    this.Name = Name;
-    this.Level = Level;
-    this.heroType = heroType;
     this.MP = MP;
     this.HP = HP;
   }
