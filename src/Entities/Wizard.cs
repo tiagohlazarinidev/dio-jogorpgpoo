16ed168a6121ef988727d6ce9057c5856e27a3fc
Tiago Lazarini <tiago_lazarini@hotmail.com>
2022-04-12 13:03:46 -0300

    Update Wizard.cs

--- src/Entities/Wizard.cs
+++ src/Entities/Wizard.cs
@@ -2,9 +2,6 @@ public class Wizard : Hero
 {
   public Wizard(string Name, int Level, string heroType, int MP, int HP) : base(Name, Level, heroType, MP, HP)
   {
-    this.Name = Name;
-    this.Level = Level;
-    this.heroType = heroType;
     this.MP = MP;
     this.HP = HP;
   }
